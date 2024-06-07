0fc181a4074c8f3f8ab128a257641e4777723ed8
Cinemahubbots <126318209+Cinemahubbots@users.noreply.github.com>
2024-06-07 15:59:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -17,10 +17,10 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # Telegram app id, obtain it from my.telegram.org
-APP_ID = 29040566
+APP_ID = 18578455
 # Telegram api hash, obtain it from my.telegram.org
-API_HASH = "9a1ed45b426b84a8e6cfc9249c0dbb4f"
+API_HASH = "26dc6e8404a1f7c6c14c4eb9051e04e9"
 # Your pyrogram session string here
-SESSION_STRING ="BAG7H7YApP41WYYbWxWZGlV1oSJTF4pCNuPeDYGo36KtMAcaGm7rCE5aXF9a6Vmmi6Riphbjz8geRRXaoEo0oqaMbXpHqEI7E99OauAZbrgsWL9WAf0XI45CYhMZ_23G_kpquAizMkDagq0u0KhvJpjE6a1bEGASXr24XgvXiU5TYCJmb_6fcHsqQV8cVGXGQQ_ub1JXANmmG1xK_dfQ66WCB1VrPn789mTlTnsze0G8pdaWUgK8D5JiD90sHkpN_KLtAh4NbISptvfJDX9q6k1CwX3sU0yY3OP1PQQWdmTD_roX2aewKEXrbPotQaeAr0lmMQYsC8nIgkOfuH7xGPZM-ZSzeQAAAAGE716QAA"
+SESSION_STRING ="BQEbfBcAQ_bD8jNzwM_CvBe-qdTvl0s1dOIuwzB8wSueXqN_G7SB27II8powrUPPWmaFv7kENSIRguYA0l2OKJ3BeJdYO9C2dNAMtzl2iLR6Gp_CcpqrEXx3xKiMgI3juxVkXrmc2l99snT-mJmLPjVbEbhqJOpD9p2N_p0GSfGnswHJ09ibFxBtHuAsNuqg0j0nYVsCBwA_CldG60S3OtlTZ-TQCRLa-AoeEL2psnsIlNsFLILzAtY3A9T6NeRS0wTtz5ZsPsUGJ_kzyAy_mZAZ6o035GC3ILYdRjSDKWZoOPOlljKpC12opj6vGoCNGlMvNOqr8EOjaAqQGofOYxJydmngbwAAAAGKidiXAA"
 # Your telegram user id
 OWNER_ID = 5531461861
